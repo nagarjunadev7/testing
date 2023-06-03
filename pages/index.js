c7c5758c7f35bca09e3d3a4e0297a301c12e33c3
Nagarjuna Dev <107834502+nagarjunadev7@users.noreply.github.com>
2023-06-03 09:48:59 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>QR Code Scanner Online </title>
+        <title>Web QR Code Scanner Online </title>
         <meta name="description" content="Scan QR codes quickly and easily with our QR code scanner online. Our user-friendly scanner is compatible with all devices and can be used for various purposes. Try it now for free." />
         <meta name="keywords" content="QRcode,Qr code scanner online,bar code scanner online,barcode scanner,qrcode scanner,qrcode scanner mobile"></meta>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
